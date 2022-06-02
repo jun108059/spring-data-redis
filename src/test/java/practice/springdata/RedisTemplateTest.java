1e4e8fabadfabcb25d6b8ff9185410b49055731c
YoungJun Park <youngjun108059@gmail.com>
2022-06-02 17:42:04 +0900

    ✅ RedisTemplate Operation 테스트

---
+++ src/test/java/practice/springdata/RedisTemplateTest.java
@@ -0,0 +1,49 @@
+package practice.springdata;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.data.redis.core.ValueOperations;
+
+@SpringBootTest
+public class RedisTemplateTest {
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @Test
+    void testStrings() {
+        // given
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        String key = "stringKey";
+
+        // when
+        valueOperations.set(key, "hello");
+
+        // then
+        String value = valueOperations.get(key);
+        assertThat(value).isEqualTo("hello");
+    }
+
+    @Test
+    void testSet() {
+        // given
+        SetOperations<String, String> setOperations = redisTemplate.opsForSet();
+        String key = "setKey";
+
+        // when
+        setOperations.add(key, "h", "e", "l", "l", "o");
+
+        // then
+        Set<String> members = setOperations.members(key);
+        Long size = setOperations.size(key);
+
+        assertThat(members).containsOnly("h", "e", "l", "o");
+        assertThat(size).isEqualTo(4);
+    }
+}
