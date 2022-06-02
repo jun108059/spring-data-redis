54d8249558a6f634d630a564fd719cf546f9d4bf
YoungJun Park <youngjun108059@gmail.com>
2022-06-02 17:40:41 +0900

    🔧 EmbeddedRedis, Redis 설정

---
+++ src/main/java/practice/springdata/config/EmbeddedRedisConfig.java
@@ -0,0 +1,39 @@
+package practice.springdata.config;
+
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+/**
+ * 로컬 환경의 경우 내장 Redis 실행
+ */
+@Slf4j
+@Profile({"local", "default"})
+@Configuration
+public class EmbeddedRedisConfig implements InitializingBean, DisposableBean {
+
+    @Value("$spring.redis.port")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @Override
+    public void destroy() {
+        Optional.ofNullable(redisServer).ifPresent(RedisServer::stop);
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        redisServer = RedisServer.builder()
+            .port(redisPort)
+            .setting("maxmemory 256M")
+            .build();
+
+        redisServer.start();
+    }
+}
