174a924f9fa96310ce52f9f3ad4b2b05d5c79276
YoungJun Park <youngjun108059@gmail.com>
2022-06-06 23:35:30 +0900

    👔 Redis POJO 직렬화 추가

--- src/main/java/practice/springdata/config/RedisConfig.java
+++ src/main/java/practice/springdata/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package practice.springdata.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +10,9 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import practice.springdata.entity.Member;
 
 @Configuration
 @EnableRedisRepositories
@@ -15,7 +21,7 @@ public class RedisConfig {
     @Value("${spring.redis.host}")
     private String redisHost;
 
-    @Value("$spring.redis.port")
+    @Value("${spring.redis.port}")
     private int redisPort;
 
     @Bean
@@ -24,10 +30,32 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<?, ?> redisTemplate() {
-        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+
         return redisTemplate;
     }
 
+    @Bean(name="redisDtoTemplate")
+    public RedisTemplate<String, Member> redisDtoTemplate() {
+        RedisTemplate<String, Member> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()));
+
+        return redisTemplate;
+    }
+
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
 }
